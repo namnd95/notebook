6f2c6bb8d1da2fe4c8806dc087e902186f11c8ea
namnd_58 <namnd_58@vnu.edu.vn>
2016-01-08 16:07:18 +0700

    Generate site map

--- generate_json.py
+++ generate_json.py
@@ -1,37 +1,44 @@
 import os
-
+import json
 NON_DIRECTORY = ['.git', 'content']
+
 
 def get_list_file(directory):
     return os.walk(directory).next()[2]
-    
+
+
 def get_list_dir(directory):
     return os.walk(directory).next()[1]
-    
+
+
 def travel(directory):
+    dict = {}
     list_dir = get_list_dir(directory)
     list_file = get_list_file(directory)
-        
-    for dir in list_dir:
-        if dir[0] != '.' and dir not in NON_DIRECTORY:
-            travel(directory+'/'+dir)
-    
+
     items = []
     for dir in list_dir:
-        items.append(dir)
+        if dir[0] != '.' and dir not in NON_DIRECTORY:
+            dict[dir] = travel(directory + '/' + dir)
+            items.append(dir)
+
     for file in list_file:
         items.append(name_part(file))
-        
+
     items.sort()
-    with open(directory+'/data.json', 'w') as f:
-        f.write('[\n')
-        for item in items:
-            f.write('%s,\n' % item)
-        f.write(']')
-        
+    dict['_items'] = items
+    return dict
+
 
 def name_part(file_name):
     return file_name[:file_name.rfind('.')]
-    
-if __name__ == '__main__':            
-    travel('content')    
+
+if __name__ == '__main__':
+    dict = travel('content')
+    with open('content/data.json', 'w') as f:
+        f.write( json.dumps(
+            dict,
+            sort_keys=True,
+            indent=4,
+            separators=(',',': ')
+        ))
